725e6312dad1d15a3dbbbf1fec70e38241b8c131
Muhammad Haziq Ahmad Kamel <haziq.kamel@abxexpress.com.my>
2022-05-31 10:19:27 +0800

    reward waver and implement delayed

--- contracts/WavePortal.sol
+++ contracts/WavePortal.sol
@@ -30,6 +30,10 @@ contract WavePortal {
     // Because we deploy the contract on blockchain, this variable will never be reset as long as the blockchain is alive
     Wave[] waves;
 
+    // This is an address => uint mapping, associate an address with a number
+    // In this case, storing the address with the last time the user waved at us
+    mapping(address => uint256) public lastWavedAt;
+
     constructor() payable {
         console.log("Learn Blockchain from BuildSpace! - haziqkamel");
         // Set the initial seed
@@ -37,6 +41,15 @@ contract WavePortal {
     }
 
     function wave(string memory _message) public {
+
+        // Make sure the current timestamp is at least 15 minutes bigger than the last timestamp we stored
+        require(
+            lastWavedAt[msg.sender] + 15 minutes < block.timestamp, "Wait 15m"
+        );
+
+        // Update the current timestamp we have for the user
+        lastWavedAt[msg.sender] = block.timestamp;
+
         totalwaves += 1;
         console.log("%s has waved! with message: %s", msg.sender, _message);
 
@@ -49,6 +62,7 @@ contract WavePortal {
 
         // Give a 50% chance that the user wins the prize.
         if (seed <= 50) {
+            console.log("%s won!", msg.sender);
             // Reward waver
             uint256 prizeAmount = 0.0001 ether;
 
